fd4c02900cfa9e78eac70925786124baa7b7b633
Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
2019-12-27 09:08:28 +0100

    vor Umbau mit zentraler bp Instanz.

--- luftstrom-display/audio-tmp.lisp
+++ luftstrom-display/audio-tmp.lisp
@@ -1,33 +1,29 @@
 (in-package :luftstrom-display)
 
-;;; audio-preset: 103
+;;; audio-preset: 0
 
 (digest-audio-args-preset
- '(:cc-state #(0 109 0 127 0 0 0 58 0 0 0 9 52 0 40 127)
-   :p1 1
+ '(:p1 1
    :p2 (- p1 1)
    :p3 0
    :p4 0
    :synth 1
-   :pitchfn (n-exp y 0.1 1)
-   :ampfn (* (sign) (n-exp y 2 1) (r-exp 0.3 4))
-   :durfn (* (/ *maxspeed*) (mc-exp 13 0.02 2) (mc-exp-dev 14 4))
-   :suswidthfn 0.2
+   :pitchfn (* (n-exp y 0.7 1.3) 0.63951963)
+   :ampfn (* (sign) (n-exp y 1 0.5))
+   :durfn (* (m-exp (mc-ref 6) 0.1 1) (r-exp 0.2 0.6))
+   :suswidthfn 0.3
    :suspanfn 0
-   :decaystartfn 0
-   :decayendfn 0.01
-   :lfofreqfn (* (mc-exp-dev 11 2) (mc-exp 12 1 80))
+   :decaystartfn 5.0e-4
+   :decayendfn 0.002
+   :lfofreqfn (* (m-exp (mc-ref 4) 0.25 1) (r-exp 45 45))
    :xposfn x
    :yposfn y
-   :wetfn (mc-lin 16 0 1)
-   :filtfreqfn (mc-exp 8 1000 10000)
-   :bpfreq (n-exp y 200 5000)
-   :vwlinterp (mcn-ref 3)
+   :wetfn (m-lin (mc-ref 8) 0 1)
+   :filtfreqfn (n-exp y 1000 10000)
    :voicepan (mcn-ref 1)
-   :vowel y
-   :voicetype (random 5)
-   :bprq (mc-exp 15 1 0.02))
- (aref *audio-presets* 103))
+   :bpfreq (n-exp y 100 5000)
+   :bprq (m-lin (mc-ref 7) 1 0.01))
+ (aref *audio-presets* 0))
 
 
 (save-audio-presets)
