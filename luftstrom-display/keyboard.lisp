0c2dd0d0756b0733bb006358dfe70e8cafb44aa0
Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
2020-07-02 19:16:17 +0200

    misc.

--- luftstrom-display/keyboard.lisp
+++ luftstrom-display/keyboard.lisp
@@ -19,6 +19,12 @@
 ;;; **********************************************************************
 
 (in-package :luftstrom-display)
+(defstruct keypgm
+  (bs-preset 0)
+  (audio t)
+  (boids t)
+  (obstacles nil)
+  (resave nil))
 
 (defclass keyboard (midi-controller)
   ((cc-offset :initform 0
@@ -35,8 +41,26 @@
   (setf (note-fn instance) (flock-keyboard-in instance)))
 
 (defun flock-keyboard-in (instance)
-  (lambda (key val)
-    (with-debugging
-      (format t "~&key-in ~S: ~a ~a~%" (id instance) key val))))
+  (let ((save-state nil))
+    (lambda (key velo)
+      (when (> velo 0)
+        (with-debugging
+          (format t "~&key-in ~S: ~a ~a~%" (id instance) key velo))
+        (if save-state t)
+        (let ((pgm (elt *keyboard-pgms* key)))
+          (bs-state-recall
+           (keypgm-bs-preset pgm)
+           :load-obstacles (keypgm-obstacles pgm)
+           :load-audio (keypgm-audio pgm)
+           :load-boids (keypgm-boids pgm)))))))
 
 ;;; (add-midi-controller 'keyboard :id :kbd1 :chan 6)
+
+(defparameter *keyboard-pgms*
+  (make-array 1024
+              :element-type 'keypgm
+              :initial-contents (loop
+                                  for pgm below 1024
+                                  collect (make-keypgm :bs-preset (mod pgm 128)))))
+
+
