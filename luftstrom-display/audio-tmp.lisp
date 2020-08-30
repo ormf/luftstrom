244a7ac4009174ba135c8b5d2a8e160b77c9d98a
Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
2020-08-30 19:04:05 +0200

    vor 3. Probe Mosaik, mit Martin Änderungen

--- luftstrom-display/audio-tmp.lisp
+++ luftstrom-display/audio-tmp.lisp
@@ -1,41 +1,47 @@
 (in-package :luftstrom-display)
 
-;;; audio-preset: 3
+;;; audio-preset: 2
 
 (digest-audio-preset-form
- '(:cc-state #(1 0 0 0 127 127 0 0 23 127 127 127 0 125 127 127)
-   :p1 (mcn-ref 6)
-   :p2 (mcn-ref 5)
+ '(:cc-state #(0 4 127 0 0 0 0 127 43 0 0 0 0 13 58 127)
+   :p1 (if (<= (mc-lin 6 0 1) (random 1.0))
+           (* (expt (min 2 (/ v)) (mcn-ref 13)) (mc-exp 14 0.1 1) (r-exp 0.2 0.6))
+           0.6)
+   :p2 (if (<= (mcn-ref 5) (random 1.0))
+           0
+           1)
    :p3 0
    :p4 0
    :synth 1
-   :pitchfn (n-exp y 0.4 (mc-lin 13 0.8 1.2))
-   :ampfn (* (sign) (n-lin p1 (+ 0.1 (random 0.6)) (n-exp y 0.7 0.35)))
-   :durfn (n-lin p1 (n-lin p2 (n-exp y 0.8 0.16) (* p2 (mc-exp 14 0.1 0.5))) 0.5)
-   :suswidthfn (n-lin p1 (* p2 0.5) 0.5)
-   :suspanfn 0.3
-   :decaystartfn (n-lin p1 (n-lin p2 0.001 0.03) 0.03)
-   :decayendfn (n-lin p1 (n-lin p2 0.02 0.03) 0.03)
-   :lfofreqfn (n-lin p1
-               (n-lin p2
-                (*
-                 (expt (1+ (round (* 15 y (mcn-ref 11))))
-                  (n-lin x 1 (mc-lin 12 1 1.5)))
-                 (hertz (mc-lin 9 31 55)))
-                (* (n-exp y 0.8 1.2) (mc-exp 9 50 400)
-                 (n-exp-dev (mcn-ref 10) 0.5)))
-               (* 12.5 (expt 2 (+ 2 (random 4)))))
+   :pitchfn (n-exp y 0.448 0.831)
+   :ampfn (* (sign) (expt (mc-exp 8 0.1 1) p2) (db->amp (rand -10)))
+   :durfn p1
+   :suswidthfn (n-lin p2 0.3 0)
+   :suspanfn (n-lin p2 0 0.3)
+   :decaystartfn 5.0e-4
+   :decayendfn 0.002
+   :lfofreqfn (* 1
+               (expt
+                (round
+                 (1+
+                  (*
+                   (if (zerop p2)
+                       1
+                       15)
+                   y (mcn-ref 11))))
+                (n-lin x 1 (mc-lin 12 1 1.5)))
+               (mtof (mc-lin 9 (n-lin p2 3.5 31) 55)) (mc-exp-dev 10 1.2))
    :xposfn x
    :yposfn y
-   :wetfn (mcn-ref 16)
-   :filtfreqfn (n-lin p1 (* (n-exp y 1 2) (mc-exp 13 100 10000)) 10000)
+   :wetfn (mc-lin 16 0 1)
+   :filtfreqfn (n-exp y (n-lin p2 1000 200) 10000)
    :vowel y
    :voicetype (random 5)
    :voicepan (mcn-ref 1)
    :bpfreq (n-exp y (n-lin p2 1000 100) 5000)
    :bprq (mc-exp 15 1 0.01)
-   :bppan (n-lin p1 (mc-lin 3 0 1) 0))
- :audio-preset (aref *audio-presets* 3))
+   :bppan (mcn-ref 3))
+ :audio-preset (aref *audio-presets* 2))
 
 
 (save-audio-presets)
